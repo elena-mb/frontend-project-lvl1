cfc87605f6ef79387e2d63a36e1fabb4f0afec76
elena-mb <elena.b.m@mail.ru>
2022-03-20 14:26:09 +0300

    Edit src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,6 @@ function introduction() {
   console.log('brain-games\nWelcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 }
 export default introduction;
