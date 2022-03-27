7fb8212d39c9c362afc0a1f30712baa3e9e91e85
elena-mb <elena.b.m@mail.ru>
2022-03-27 19:31:15 +0300

    Add new game (brain-prime)

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 ![Node CI](https://github.com/elena-mb/frontend-project-lvl1/actions/workflows/github-actions.yml/badge.svg)
 
 Аскинема с примером запуска игры, победой и поражением игрока:
-[![asciicast](https://asciinema.org/a/1aibqArH09EGOOaiGBhQ8lzva.svg)](https://asciinema.org/a/1aibqArH09EGOOaiGBhQ8lzva)
+[![asciicast](https://asciinema.org/a/edeIJNAabNfmkcN1VelgGInqJ.svg)](https://asciinema.org/a/edeIJNAabNfmkcN1VelgGInqJ)
---
+++ bin/brain-prime.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import playTheGame from '../src/index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const maxNum = 15;
+const getRandomNum = (max) => Math.floor(Math.random() * max) + 1;
+
+function game() {
+  const num = getRandomNum(maxNum);
+
+  const isPrime = (a) => {
+    if (a <= 2) return true;
+    if (a % 2 === 0) return false;
+    for (let i = 3; i < Math.sqrt(a); i += 2) {
+      if (a % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const answer = isPrime(num) ? 'yes' : 'no';
+  return {
+    question: `${num}`,
+    rightAnswer: `${answer}`,
+  };
+}
+
+playTheGame(game, description);
