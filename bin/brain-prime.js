c66423107435c8483fa67cb9eac5e869f632565c
elena-mb <elena.b.m@mail.ru>
2022-03-27 20:52:01 +0300

    Edit bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -12,7 +12,7 @@ function game() {
   const isPrime = (a) => {
     if (a === 2) return true;
     if (a < 2 || a % 2 === 0) return false;
-    for (let i = 3; i < Math.sqrt(a); i += 2) {
+    for (let i = 3; i <= Math.sqrt(a); i += 2) {
       if (a % i === 0) {
         return false;
       }
