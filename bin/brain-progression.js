6ff63d327307e529a66689d650925fb63f2f9cea
elena-mb <elena.b.m@mail.ru>
2022-03-29 20:17:21 +0300

    Refactor games

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -7,13 +7,12 @@ const description = 'What number is missing in the progression?';
 const maxNum = 30;
 const minNum = 1;
 const stepDifference = 8;
-// const getRandomNum = (max) => Math.floor(Math.random() * max);
 
 function game() {
   const firstNum = getRandomNum(minNum, maxNum);
   const step = getRandomNum(minNum, stepDifference);
   const progression = [firstNum];
-  for (let i = 1; i < 10; i += 1) {
+  for (let i = progression.length; i < 10; i += 1) {
     progression[i] = progression[i - 1] + step;
   }
   const missingIndex = getRandomNum(0, progression.length);
